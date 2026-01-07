7058671b570f4e8c07ab64907291c4d10c71c39e
fedev <fedevgd@gmail.com>
2026-01-07 02:03:16 -0300

    Prueba de auto en main

--- pruebaConsola/Program.cs
+++ pruebaConsola/Program.cs
@@ -1,5 +1,10 @@
 ﻿using PersonaNamespace;
+using AutoNamespace;
 
 Persona persona1 = new Persona("Juan", 30);
 Console.WriteLine($"Nombre: {persona1.Nombre}, Edad: {persona1.Edad}");
+
+Auto auto1 = new Auto("Toyota", "JKU183", 2026, "Rojo", 40000000f);
+
+Console.WriteLine($"Marca: {auto1.Marca}, Patente: {auto1.Patente}, Año: {auto1.Año}, Color: {auto1.Color}, Precio: {auto1.Precio}");
 
